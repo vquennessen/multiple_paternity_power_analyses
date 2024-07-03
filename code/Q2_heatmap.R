3874ffd4a2beada9cc1ff85ef50f200c27e9aeb4
vquennessen <viquennessen@gmail.com>
2024-07-03 10:18:40 -0500

    adjusted paths for Q2_heatmap code

--- code/Q2_heatmap.R
+++ code/Q2_heatmap.R
@@ -1,7 +1,7 @@
 # Q2 heatmaps
 
 # set working directory
-setwd("~/Projects/iliketurtles3/code/power analysis")
+setwd("~/Projects/power analysis")
 
 # load libraries
 library(dplyr)
@@ -10,23 +10,29 @@ library(viridisLite)
 library(patchwork)
 
 # model parameters
+# fertilization modes
 fmodes <- c('random', 
             'exponential',
-            # 'dominant50',
-            # 'dominant70',
-            # 'dominant90',
+            'dominant50',
+            'dominant70',
+            'dominant90',
             'mixed_dominant')
 
+# fertilization mode titles
 fmode_titles <- c('Random', 'Exponential', 
-                  # 'Dominant 50', 'Dominant 70', 'Dominant 90', 
+                  'Dominant 50', 'Dominant 70', 'Dominant 90', 
                   'Mixed Dominant')
 
+# sample sizes
 sample_sizes <- c(32, 96)
+
+# number of simulations
+nsims <- 1e+05
 
 # dimensions
 nS <- length(sample_sizes)
 
-# initialize list of dataframes
+# initialize dataframe
 DF <- data.frame()
 
 # for each fertilization mode
@@ -40,54 +46,9 @@ for (f in 1:length(fmodes)) {
     sample_size <- sample_sizes[s]
     
     # load data
-    load(paste('~/Projects/iliketurtles3/code/power analysis/', sample_size, 
-               '_nests_to_sample_', fmode, '_100.Rdata', sep = ''))
+    load(paste('~/Projects/power analysis/output', sample_size, 
+               '_nests_to_sample_', fmode, '_', nsims, '.Rdata', sep = ''))
     DF1 <- output
-    
-    # # dimensions
-    # nBSR <- n_distinct(DF1$BSR)
-    # nPN <- n_distinct(DF1$PropNests)
-    
-    ################################################################################
-    
-    # # save heatmap
-    # ggsave(fig32, 
-    #        file = 'C://Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/chapters/chapter 1/figures/32_mixed_dominant.png', 
-    #        height = 4, width = 6)
-    ################################################################################
-    
-    # # sample size 96 heatmap
-    # fig96 <- ggplot(data = DF96, aes(x = PropNests, y = BSR, fill = Proportion)) +
-    #   geom_tile(color = 'white',
-    #             lwd = 1.5,
-    #             linetype = 1) +
-    #   scale_fill_gradient2(low = hcl.colors(n = 5)[1],
-    #                        mid = hcl.colors(n = 5)[3],
-    #                        high = hcl.colors(n = 5)[5],
-    #                        midpoint = 0.5,
-    #                        breaks = c(0, 0.25, 0.5, 0.75, 1),
-    #                        limits = c(0, 1),
-    #                        na.value = 'gray') +
-    #   xlab('Proportion of Nests Sampled') +
-    #   ylab('Breeding Sex Ratio') +
-    #   labs(fill = 'Proportion \n') +
-    #   # geom_text(aes(label = round(Proportion, 2))) +
-    #   ggtitle('b. Sample size 96') +
-    #   theme(text = element_text(size = 15))
-    
-    # # save heatmap
-    # ggsave(fig96, 
-    #        file = 'C://Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/chapters/chapter 1/figures/96_mixed_dominant.pdf', 
-    #        height = 4, width = 6)
-    
-    # # put heatmaps together
-    # A <- fig32 + theme(legend.position = 'none')
-    # B <- fig96 + ylab('') + theme(axis.text.y = element_blank())
-    # 
-    # patchwork <- A + B + 
-    #   xlab('Proportion of Nests Sampled') +
-    #   ggtitle(fmode)
-    # patchwork
     
     # fertilization mode and sample size vectors
     Fertilization_Mode <- factor(rep(fmode_title, nrow(DF1)))
@@ -131,3 +92,47 @@ fig4 <- ggplot(data = DF, aes(x = PropNests, y = OSR, fill = Proportion)) +
 
 fig4
 
+# # dimensions
+# nBSR <- n_distinct(DF1$BSR)
+# nPN <- n_distinct(DF1$PropNests)
+
+################################################################################
+
+# # save heatmap
+# ggsave(fig32, 
+#        file = 'C://Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/chapters/chapter 1/figures/32_mixed_dominant.png', 
+#        height = 4, width = 6)
+################################################################################
+
+# # sample size 96 heatmap
+# fig96 <- ggplot(data = DF96, aes(x = PropNests, y = BSR, fill = Proportion)) +
+#   geom_tile(color = 'white',
+#             lwd = 1.5,
+#             linetype = 1) +
+#   scale_fill_gradient2(low = hcl.colors(n = 5)[1],
+#                        mid = hcl.colors(n = 5)[3],
+#                        high = hcl.colors(n = 5)[5],
+#                        midpoint = 0.5,
+#                        breaks = c(0, 0.25, 0.5, 0.75, 1),
+#                        limits = c(0, 1),
+#                        na.value = 'gray') +
+#   xlab('Proportion of Nests Sampled') +
+#   ylab('Breeding Sex Ratio') +
+#   labs(fill = 'Proportion \n') +
+#   # geom_text(aes(label = round(Proportion, 2))) +
+#   ggtitle('b. Sample size 96') +
+#   theme(text = element_text(size = 15))
+
+# # save heatmap
+# ggsave(fig96, 
+#        file = 'C://Users/vique/Box Sync/Quennessen_Thesis/PhD Thesis/chapters/chapter 1/figures/96_mixed_dominant.pdf', 
+#        height = 4, width = 6)
+
+# # put heatmaps together
+# A <- fig32 + theme(legend.position = 'none')
+# B <- fig96 + ylab('') + theme(axis.text.y = element_blank())
+# 
+# patchwork <- A + B + 
+#   xlab('Proportion of Nests Sampled') +
+#   ggtitle(fmode)
+# patchwork
