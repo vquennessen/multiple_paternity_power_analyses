ee0211a6ee0121e386910319eaf7649eb5c15fe5
vquennessen <viquennessen@gmail.com>
2024-08-08 09:58:00 -0700

    working on bayes thing

--- code/bayes_probs.R
+++ code/bayes_probs.R
@@ -1,17 +1,57 @@
-bayes_probs <- function(number_of_males, fertilization_modes, max_males) {
+bayes_probs <- function(number_of_males, 
+                        fertilization_modes, 
+                        max_males, 
+                        fertilization_mode = NULL, 
+                        males_identified = NULL, 
+                        sample_size = NULL) {
   
-  # dimentions
+  # dimensions
   FM <- length(fertilization_modes)
   MM <- max_males
   
-  # initialize dataframe
-  bayes_probs_DF <- data.frame(Fertilization_mode = rep(fertilization_modes, 
-                                                        each = MM*MM), 
-                               Males_detected = rep(c(1:max_males), 
-                                                    times = FM, each = MM),
-                               Males_contributed = rep(c(1:max_males), 
-                                                       times = FM*MM),
-                               Probability = NA)
+  # load in results
+  load(number_of_males)
   
+  # sample sizes
+  sample_sizes <- unique(number_of_males$Sample_size)
+  
+  if (is.null(fertilization_mode) & 
+      is.null(males_identified) & 
+      is.null(sample_size)) {
+    
+    # initialize dataframe
+    bayes_probs_DF <- data.frame()
+    
+    for (f in 1:length(fertilization_modes)) {
+      
+      for (m in 1:max_males) {
+        
+        for (s in 1:length(sample_sizes)) {
+          
+          # pull out fertilization mode and the number of males detected
+          subset <- number_of_males %>%
+            filter(Fertilization_Mode == fertilization_modes[f]) %>%
+            filter(Males_detected == m) %>%
+            filter(Sample_size == sample_sizes[s])
+          
+          # add 
+          rbind(bayes_probs_DF, subset)
+          
+        }
+        
+      }
+      
+    }
+    
+    
+  } else {
+    
+    output <- number_of_males %>%
+      filter(Fertilization_Mode == fertilization_mode) %>%
+      filter(Males_identified == males_identified)
+    
+  }
+  
+  return(output)
   
 }
